ed50cf73822391e25804e90ca7594ba3b528cacf
bhuvanroyal <damabhuvan@gmail.com>
2023-08-05 11:02:11 +0000

    medication class added

--- .github/workflows/pharmacy-service/src/main/java/com/virtusa/pharmacyservice/entity/Medication.java
+++ .github/workflows/pharmacy-service/src/main/java/com/virtusa/pharmacyservice/entity/Medication.java
@@ -1,11 +1,48 @@
 package com.virtusa.pharmacyservice.entity;
+import jakarta.persistence.Entity;
 
+@Entity
 public class Medication {
 
     int id;
-    String name;
-    String description;
-    String availability;
+	 String name;
+	 String description;
+	 String availability;
+	 
+	public Medication() {
+		super();
+	}
+	public Medication(int id, String name, String description, String availability) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.description = description;
+		this.availability = availability;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getAvailability() {
+		return availability;
+	}
+	public void setAvailability(String availability) {
+		this.availability = availability;
+	}
     
     
 }
